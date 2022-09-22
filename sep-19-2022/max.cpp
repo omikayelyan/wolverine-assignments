c7fba541efefc199646e58a008c661408d3f335f
omikayelyan <104993462+omikayelyan@users.noreply.github.com>
2022-09-22 13:48:04 +0400

    Update max.cpp

--- sep-19-2022/max.cpp
+++ sep-19-2022/max.cpp
@@ -32,3 +32,45 @@ main:
         mov     eax, 0
         pop     rbp
         ret
+
+int max(int num1, int num2)
+{
+    int max;
+    max = num1;
+    if (num2 > max)
+    {
+        max = num2;
+    }
+    return max;
+}
+
+int main()
+{
+    std::cout << max(4, 9);
+}
+
+max(int, int):
+        push    rbp
+        mov     rbp, rsp
+        mov     DWORD PTR [rbp-20], edi
+        mov     DWORD PTR [rbp-24], esi
+        mov     eax, DWORD PTR [rbp-20]
+        mov     DWORD PTR [rbp-4], eax
+        mov     eax, DWORD PTR [rbp-24]
+        cmp     eax, DWORD PTR [rbp-4]
+        jle     .L2
+        mov     eax, DWORD PTR [rbp-24]
+        mov     DWORD PTR [rbp-4], eax
+.L2:
+        mov     eax, DWORD PTR [rbp-4]
+        pop     rbp
+        ret
+main:
+        push    rbp
+        mov     rbp, rsp
+        mov     esi, 9
+        mov     edi, 4
+        call    max(int, int)
+        mov     eax, 0
+        pop     rbp
+        ret
