cf62c48148cbdc5d75a107337cd9d7eb4a9ec76f
omikayelyan <104993462+omikayelyan@users.noreply.github.com>
2022-09-27 23:11:14 +0400

    Update split_2.cpp

--- sep-27-2022/split_2.cpp
+++ sep-27-2022/split_2.cpp
@@ -1,1 +1,36 @@
+// split(s, d); որը կվերադարձնի տողերի զանգված, որտեղ յուրաքանչյուր տող ստացվել է s տողը կտրտելով ըստ տրված d բաժանիչների, այսինքն՝
+// split("he,ll,o", ","); կվերադարձնի ["he", "ll", "o"], իսկ split("he ,ll:o world,vui", " ,:"); կվերադարձնի ["he", "ll", "o", "world", "vui"]։
+// Փոխանցվող բաժանիչների քանակի սահմանափակում չկա։
 
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::vector <std::string> split(std::string& s, std::string& sym)
+{
+    std::vector <std::string> vec;
+    std::string str = "";
+    for (int i = 0; i < s.size(); ++i)
+    {
+        if (sym.find(s[i]) != -1)
+        {
+            vec.push_back(str);
+            str = "";
+            continue;
+        }
+        str.push_back(s[i]);
+    }
+    vec.push_back(str);
+    return vec;
+}
+
+int main()
+{
+    std::string s = "he ,ll:o world,vui";
+    std::string sym = " ,,:";
+    std::vector <std::string> vec = split(s, sym);
+    for (auto i : vec)
+    {
+        std::cout << i << " ";
+    }
+}
