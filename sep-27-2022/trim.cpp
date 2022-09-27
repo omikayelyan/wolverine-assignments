42d670f2659b395255c7736244e36b0ac8ae4061
omikayelyan <104993462+omikayelyan@users.noreply.github.com>
2022-09-27 22:14:08 +0400

    Create trim.cpp

---
+++ sep-27-2022/trim.cpp
@@ -0,0 +1,42 @@
+// trim(s); որը վերադարձնում է տողը առանց ձախից և աջից բացատների trim("  hello  "); կվերադարձնի "hello"
+
+#include <iostream>
+#include <string>
+
+std::string trim(std::string& s)
+{
+    int count = 0;
+    for (int i = s.size(); i >= 0; --i)
+    {
+        if (s[i] == ' ')
+        {
+            ++count;
+        }
+        else
+        {
+            break;
+        }
+    }
+    int size = s.size() - count;
+    s.erase(size, s.size());
+    int count1 = 0;
+    for (int i = 0; i < s.size(); ++i)
+    {
+        if (s[i] == ' ')
+        {
+            ++count1;
+        }
+        else
+        {
+            break;
+        }
+    }
+    s.erase(0, count1);
+    return s;
+}
+
+int main()
+{
+    std::string s = "   hello   ";
+    std::cout << trim(s);
+} 
