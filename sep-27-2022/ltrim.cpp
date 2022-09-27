71e88f802af9872a194f74298903a39c59f125bc
omikayelyan <104993462+omikayelyan@users.noreply.github.com>
2022-09-27 21:00:39 +0400

    Update ltrim.cpp

--- sep-27-2022/ltrim.cpp
+++ sep-27-2022/ltrim.cpp
@@ -2,3 +2,23 @@
 
 #include <iostream>
 #include <string>
+
+std::string ltrim(std::string& s)
+{
+    int count = 0;
+    for (int i = 0; i < s.size(); ++i)
+    {
+        if (s[i] == ' ')
+        {
+            ++count;
+        }
+    }
+    s.erase(0, count);
+    return s;
+}
+
+int main()
+{
+    std::string s = "    hello";
+    std::cout << ltrim(s);
+}
