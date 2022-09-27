39c4af9e9c26efd4e57bf2e99eab02efc8d991ce
omikayelyan <104993462+omikayelyan@users.noreply.github.com>
2022-09-27 23:41:22 +0400

    Update upperCase.cpp

--- sep-28-2022/upperCase.cpp
+++ sep-28-2022/upperCase.cpp
@@ -1,1 +1,20 @@
+// toUpperCase(s); վերադարձնում է տողը բոլոր տառերը մեծատառ փոխված
 
+#include <iostream>
+#include <string>
+
+std::string toUpperCase(std::string& s)
+{
+    for (int i = 0; i < s.size(); ++i)
+    {
+        s[i] -= 32;
+    }
+    return s;
+}
+
+int main()
+{
+    std::string s;
+    std::cin >> s;
+    std::cout << toUpperCase(s);
+}
