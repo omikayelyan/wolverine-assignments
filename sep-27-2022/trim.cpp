0b428e1831d1d3f00381284b37e152852f47d3a9
omikayelyan <104993462+omikayelyan@users.noreply.github.com>
2022-09-29 18:35:26 +0400

    Update trim.cpp

--- sep-27-2022/trim.cpp
+++ sep-27-2022/trim.cpp
@@ -6,7 +6,7 @@
 std::string trim(std::string& s)
 {
     int count = 0;
-    for (int i = s.size(); i >= 0; --i)
+    for (int i = s.size() - 1; i >= 0; --i)
     {
         if (s[i] == ' ')
         {
